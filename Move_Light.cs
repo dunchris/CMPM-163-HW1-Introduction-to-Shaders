649fd8d32f6f0c2fed6cfc6b7d2afdc2e2824fc3
dunchris <37026447+dunchris@users.noreply.github.com>
2019-04-28 14:56:30 -0700

    Add C# scripts

---
+++ Move_Light.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Light : MonoBehaviour
+{
+    Transform trans;
+    float rad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = gameObject.GetComponent<Transform>();
+        rad = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move lights based on sine function
+        rad = Mathf.Sin((1.0f*Time.time) % 2*Mathf.PI);
+        trans.position = new Vector3(trans.position.x + rad, trans.position.y, trans.position.z);
+    }
+}

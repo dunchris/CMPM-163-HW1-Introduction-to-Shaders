bc7761342f9a6151e5e2889502348a2f114a2d9f
dunchris <37026447+dunchris@users.noreply.github.com>
2019-04-30 11:05:15 -0700

    Mouse Input for HW1B

---
+++ HW1B_Mouse_Input.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HW1B_Mouse_Input : MonoBehaviour
+{
+    Renderer render;
+    float i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+        render = GetComponent<Renderer>();
+
+        render.material.shader = Shader.Find("CMPM131HW1/Mosquito Shader");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            i += 1;
+            Debug.Log(i);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            i -= 1;
+            if (i < 1)
+                i = 1;
+            Debug.Log(i);
+        }
+
+
+        render.material.SetFloat("_mX", Input.mousePosition.x);
+        render.material.SetFloat("_mY", Input.mousePosition.y);
+        render.material.SetFloat("_MouseButtonPressed", i);
+
+
+    }
+}
